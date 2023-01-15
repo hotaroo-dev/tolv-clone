7c0364db5328445d18f029fb5c760ebb5063517a
Rolo-coding <kay.sothearo2019@rupp.edu.kh>
2023-01-15 23:13:43 +0700

    chore(src): update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,46 @@
 import React from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Search } from './svg'
 
 const Header: React.FC = () => {
+  const { pathname } = useLocation()
   const { scrollY } = useScroll()
   const backgroundColor = useTransform(
     scrollY,
-    [0, 60],
+    [0, 40],
     ['rgba(250, 250, 250, 0)', 'rgba(255, 255, 255, 1)']
   )
-  const boxShadow = useTransform(scrollY, [0, 40], ['none', '0 5px 10px #0005'])
+  const boxShadow = useTransform(scrollY, [0, 40], ['none', '0 5px 10px #0003'])
 
   return (
-    <motion.header className="header" style={{ backgroundColor, boxShadow }}>
+    <motion.header style={{ backgroundColor, boxShadow }}>
       <div className="wrapper">
         <nav className="nav">
-          <ul className="tabs">
+          <ul className="tabs normal-text">
             <li>
-              <Link to="/products">Products</Link>
+              <Link
+                to="/products"
+                className={`${pathname === '/products' ? 'active' : ''}`}
+              >
+                Products
+              </Link>
             </li>
             <li>
-              <Link to="/stockists">Stockists</Link>
+              <Link
+                to="/stockists"
+                className={`${pathname === '/stockists' ? 'active' : ''}`}
+              >
+                Stockists
+              </Link>
             </li>
             <li>
-              <Link to="/about">About us</Link>
+              <Link
+                to="/about"
+                className={`${pathname === '/about' ? 'active' : ''}`}
+              >
+                About us
+              </Link>
             </li>
           </ul>
         </nav>
