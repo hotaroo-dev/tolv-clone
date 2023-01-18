b14aea398bfc14eb936502f13d42bcd6998875d8
Rolo-coding <kay.sothearo2019@rupp.edu.kh>
2023-01-18 16:11:53 +0700

    chore(src): update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { Link, useLocation } from 'react-router-dom'
-import { Search } from './svg'
+import { Exit, Search } from './svg'
+import { spring } from './hero'
+
+const menuVariants = {
+  expanded: {
+    x: 0,
+    display: 'block'
+  },
+  collapsed: {
+    x: '-100%',
+    transitionEnd: {
+      display: 'none'
+    }
+  }
+}
 
 const Header: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState(false)
   const { pathname } = useLocation()
   const { scrollY } = useScroll()
   const backgroundColor = useTransform(
     scrollY,
-    [0, 40],
+    [0, 60],
     ['rgba(250, 250, 250, 0)', 'rgba(255, 255, 255, 1)']
   )
   const boxShadow = useTransform(scrollY, [0, 40], ['none', '0 5px 10px #0003'])
+
+  const toggleMenu = () => {
+    setOpenMenu(prev => !prev)
+  }
+
+  useEffect(() => {
+    if (!openMenu) return
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [openMenu])
 
   return (
     <motion.header style={{ backgroundColor, boxShadow }}>
@@ -44,14 +72,55 @@ const Header: React.FC = () => {
             </li>
           </ul>
         </nav>
-        <nav className="mobile-nav"></nav>
+
+        <motion.div
+          className="mobile-nav"
+          variants={menuVariants}
+          initial={false}
+          animate={openMenu ? 'expanded' : 'collapsed'}
+          transition={spring}
+        >
+          <nav className="mobile-menu">
+            <ul className="normal-text flex-1">
+              <li>
+                <Link to="/products" onClick={toggleMenu}>
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link to="/stockists" onClick={toggleMenu}>
+                  Stockists
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={toggleMenu}>
+                  About us
+                </Link>
+              </li>
+            </ul>
+            <div className="exit" onClick={toggleMenu}>
+              <Exit />
+            </div>
+            <div className="search">
+              <Search />
+              <span className="text-lg font-sans">Search anything here</span>
+            </div>
+          </nav>
+        </motion.div>
+
         <div className="logo">
           <Link to="/">
             T<span>o</span>lv
           </Link>
         </div>
+
         <div className="search">
           <Search />
+        </div>
+
+        <div className="menu-bar" onClick={() => setOpenMenu(true)}>
+          <span className="line"></span>
+          <span className="line"></span>
         </div>
       </div>
     </motion.header>
