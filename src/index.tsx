fec3016730f0ea3aea0f071b9625e3a7ee7737f5
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-03-08 09:34:44 +0700

    fix(*): forget /

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
       <RecoilRoot>
-        <BrowserRouter basename="/tolv-clone">
+        <BrowserRouter basename="/tolv-clone/">
           <ScrollToTop />
           <Router />
         </BrowserRouter>
